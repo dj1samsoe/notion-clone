9618fb2e317fc090c0602a2250aefb90a1ddb11d
dj1samsoe <dhanyh86@gmail.com>
2024-02-07 07:19:57 +0700

    feat: add convex setup

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,7 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as document from "../document.js";
+import type * as documents from "../documents.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,7 +25,7 @@ import type * as document from "../document.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  document: typeof document;
+  documents: typeof documents;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://novel-chimp-55.clerk.accounts.dev",
+      domain: "https://bursting-eel-3.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
