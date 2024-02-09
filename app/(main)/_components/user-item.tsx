bb625b9c0b7cef16864ca3a54bf5e5d1a37b268d
dj1samsoe <dhanyh86@gmail.com>
2024-02-09 23:48:22 +0700

    fix: bug in Sign out Button

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -58,9 +58,11 @@ export const UserItem = () => {
           </div>
         </div>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="w-full cursor-pointer text-muted-foreground">
-          <SignOutButton>Sign out</SignOutButton>
-        </DropdownMenuItem>
+        <SignOutButton>
+          <DropdownMenuItem className="w-full cursor-pointer text-muted-foreground">
+            Log out
+          </DropdownMenuItem>
+        </SignOutButton>
       </DropdownMenuContent>
     </DropdownMenu>
   );
